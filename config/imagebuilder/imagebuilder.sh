76b086bcbba80a48139b2eedd9f22ca14e6b25b3
wishtoday <137035683+wishteddy@users.noreply.github.com>
2025-06-16 14:25:47 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -190,7 +190,7 @@ rebuild_firmware() {
         luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
         luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
         \
-        luci-app-amlogic luci-i18n-amlogic-zh-cn \
+        \
         \
         ${config_list} \
         "
